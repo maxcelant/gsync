56a3cd36b0d8c7dadc6b515c637752edb747a76e
Max Celant <maxcelant@gmail.com>
2026-03-03 12:04:24 -0600

    chore: refactoring to add expand to provider specific package

--- internal/providers/gitlab.go
+++ internal/providers/gitlab.go
@@ -37,6 +37,68 @@ func (g gitlabEntry) Repo() string      { return g.RepoStr }
 func (g gitlabEntry) URL() string       { return g.WebURL }
 func (g gitlabEntry) CreatedAt() string { return g.CreatedAtStr }
 
+func (gp *GitLabProvider) fetchGroupProjects(group string) ([]string, error) {
+	encoded := strings.ReplaceAll(group, "/", "%2F")
+	baseURL := fmt.Sprintf("%s/api/v4/groups/%s/projects?include_subgroups=true&per_page=100", gp.cfg.BaseURL, encoded)
+
+	var projects []string
+	nextURL := baseURL
+
+	for nextURL != "" {
+		req, err := http.NewRequest(http.MethodGet, nextURL, nil)
+		if err != nil {
+			return nil, fmt.Errorf("building request for group %s: %w", group, err)
+		}
+		req.Header.Set("Authorization", "Bearer "+gp.cfg.Token)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("GET %s: %w", nextURL, err)
+		}
+		body, _ := io.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("GitLab API returned %d for group %s: %s", resp.StatusCode, group, body)
+		}
+
+		var page []struct {
+			PathWithNamespace string `json:"path_with_namespace"`
+		}
+		if err := json.Unmarshal(body, &page); err != nil {
+			return nil, fmt.Errorf("decoding group projects response: %w", err)
+		}
+		for _, proj := range page {
+			projects = append(projects, proj.PathWithNamespace)
+		}
+
+		nextURL = resp.Header.Get("X-Next-Page")
+		if nextURL != "" {
+			nextURL = fmt.Sprintf("%s/api/v4/groups/%s/projects?include_subgroups=true&per_page=100&page=%s", gp.cfg.BaseURL, encoded, nextURL)
+		}
+	}
+
+	return projects, nil
+}
+
+func (gp *GitLabProvider) Expand(repos []string) ([]string, error) {
+	var expanded []string
+	for _, r := range repos {
+		if !strings.HasSuffix(r, "/*") {
+			expanded = append(expanded, r)
+			continue
+		}
+		group := strings.TrimSuffix(r, "/*")
+		projects, err := gp.fetchGroupProjects(group)
+		if err != nil {
+			return nil, fmt.Errorf("expanding wildcard %s: %w", r, err)
+		}
+		expanded = append(expanded, projects...)
+	}
+	return expanded, nil
+}
+
 func (gp *GitLabProvider) Call(repo, author string, from time.Time) ([]Entry, error) {
 	encoded := strings.ReplaceAll(repo, "/", "%2F")
 	base := fmt.Sprintf("%s/api/v4/projects/%s/merge_requests", gp.cfg.BaseURL, encoded)
